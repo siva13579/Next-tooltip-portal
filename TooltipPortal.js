681c4d5c479faa2f184940d233fecc2c80c838ab
siva13579 <78464089+siva13579@users.noreply.github.com>
2024-07-07 17:15:38 +0530

    Update and rename README.md to TooltipPortal.js

---
+++ TooltipPortal.js
@@ -0,0 +1,15 @@
+# Next-tooltip-portal
+import ReactDOM from 'react-dom';
+
+const portalRoot = typeof document !== 'undefined' ? document.getElementById('tooltip-portal') : null;
+
+function TooltipPortal({ children }) {
+  if (!portalRoot) return null;
+  
+  return ReactDOM.createPortal(
+    children,
+    portalRoot
+  );
+}
+
+export default TooltipPortal;
